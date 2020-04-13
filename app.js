4638b6911cf60273509ead7fc45f2853b01bef37
MaikolSoro <46753453+MaikolSoro@users.noreply.github.com>
2020-04-13 13:06:57 -0600

    Añadiendo configuración de Headers a nuestro servidor

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ app.use(bodyParser.json());
 
 
 // configure header HTTP
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 //....
 
 // router Basic
